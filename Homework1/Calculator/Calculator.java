7a6f1c5999135946b22d38bce56103786aad8fac
unter <unter83@gmail.com>
2023-10-08 22:10:58 +0300

    begin of hw1

---
+++ Homework1/Calculator/Calculator.java
@@ -0,0 +1,60 @@
+package Calculator;
+
+import org.assertj.core.api.Assertions;
+
+import static org.assertj.core.api.Assertions.*;
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+    // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
+    // необходимые проверки для него используя граничные случаи
+    public static double squareRootExtraction(double num) {
+        //  0
+        //  Отрицательные числа
+        //  Дробные значения корней
+        //  Целые
+            if(num < 0) {
+                throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+            }
+            return Math.sqrt(num);
+    }
+
+    // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
+    // Примерная сигнатура и тело метода:
+
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount)  throws  ArithmeticException{
+        // purchaseAmount - сумма покупки
+        // discountAmount - размер скидки
+
+            assertThat(purchaseAmount).isNotZero();
+            assertThat(purchaseAmount).isPositive();
+            assertThat(discountAmount).isPositive();
+            assertThat(discountAmount).isLessThan(1);
+
+        return 0; // Метод должен возвращать сумму покупки со скидкой
+    }
+}
